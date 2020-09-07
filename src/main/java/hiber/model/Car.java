0e2081a346bb79fece389c86b22c70141823eeb5
user <Relect@bk.ru>
2020-09-07 17:41:48 +0300

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,60 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne ()
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "series")
+    private int series;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public Car() {}
+
+    public Car(String name, int series) {
+        this.name = name;
+        this.series = series;
+    }
+}
